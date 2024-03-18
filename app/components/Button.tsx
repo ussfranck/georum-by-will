0095df0a482bb0cc95151ebacea5fceaa71c0813
USS_FRANCK <franckmekoulou.dev@hotmail.com>
2024-03-18 09:31:59 +0100

    introduction page ~ ok

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,14 +1,23 @@
 import React, { ReactNode } from "react";
 import { noto } from "../lib/fonts";
+import Link from "next/link";
+
 
 interface ButtonProps {
   title: string;
   classname?: string;
   icon?: ReactNode;
+  type: "button" | "link";
+  handleFunction?: () => void;
 }
-export const Button : React.FC<ButtonProps> = function ( { title, classname, icon } ) {
+export const Button: React.FC<ButtonProps> = function ({ title, classname, icon, type, handleFunction }) {
   return (
-    <button type="button" className={`${noto.className} button primary-button ${classname}`}>
+    type === "link" ? (
+      <Link type="button" href={""} className={`${noto.className} button primary-button ${classname}`}>
+        <span>{title}</span>
+        {icon}
+      </Link>
+    ) : <button onClick={() => handleFunction} type="button" className={`${noto.className} button primary-button ${classname}`}>
       <span>{title}</span>
       {icon}
     </button>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,49 @@
 import React from "react";
+import styles from "./css/Introduction.module.css";
+import { Button } from "./components/Button";
+import { AppLogo } from "./components/AppLogo";
 
 export default function Home() {
+
+  const initializeLocalStorageOnConfirm = function (factor: "client" | "pro") {
+    // logic's
+    if (factor === "pro") {
+      // Local storage configs
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   return (
     <React.Fragment>
-      <main>
-        <h1>Introduction Page</h1>
-      </main>
+      <section className={`${styles.introduction_hero} flex`}>
+        <AppLogo/>
+        <div className={`${styles.introduction_hero__heading} flex`}>
+          <h1>Bienvenu sur Georum.</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit exercitationem, assumenda soluta error architecto inventore ipsum itaque sed, delectus non adipisci. Itaque tenetur assumenda libero expedita facilis, at officia quae!
+          </p>
+        </div>
+        <div className={`${styles.introduction_hero__content} grid`}>
+          <div className={`flex`}>
+            <h2>Je suis un Client</h2>
+            <p>
+              Itaque tenetur assumenda libero expedita facilis, at officia quae!  Suscipit exercitationem, assumenda soluta error architecto
+            </p>
+            <p>sit amet consectetur adipisicing elit.</p>
+            <Button title="Je suis un Client." type="button" />
+          </div>
+          <div className={`flex`}>
+            <h2>Je suis un professionel</h2>
+            <p>
+              Itaque tenetur assumenda libero expedita facilis, at officia quae!  Suscipit exercitationem, assumenda soluta error architecto
+            </p>
+            <p>sit amet consectetur adipisicing elit.</p>
+            <Button title="Je suis Professionel." handleFunction={() => initializeLocalStorageOnConfirm("pro")} type="button" />
+          </div>
+        </div>
+      </section>
     </React.Fragment>
   );
 }
